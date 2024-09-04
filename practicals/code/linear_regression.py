4d0b9bacea79896b178407f594c8154ab700a855
JeroenFrie <j.a.w.frieling@student.tue.nl>
2024-09-04 15:33:48 +0200

    Ik heb een start gemaakt aan de linear regression afmaken

--- practicals/code/linear_regression.py
+++ practicals/code/linear_regression.py
@@ -1,4 +1,6 @@
 import numpy as np
+from sklearn.datasets import load_diabetes
+
 
 def lsq(X, y):
     """
@@ -15,4 +17,37 @@ def lsq(X, y):
     # calculate the coefficients
     beta = np.dot(np.linalg.inv(np.dot(X.T, X)), np.dot(X.T, y))
 
+
     return beta
+
+def y_output(X, beta):
+    ones = np.ones((len(X), 1))
+    X = np.concatenate((ones, X), axis=1)
+
+    y_out = np.dot(X, beta)
+    return y_out
+
+def MSE(y, y_out):
+
+    error_mat = (y - y_out)
+    error_mat = np.square(error_mat)
+
+    n = error_mat.shape[0]
+    sum = np.sum(error_mat)
+    mse = (1/n) * sum
+    return mse
+
+diabetes = load_diabetes()
+X_train = diabetes.data[:300, :]
+y_train = diabetes.target[:300, np.newaxis]
+X_test = diabetes.data[300:, :]
+y_test = diabetes.target[300:, np.newaxis]
+
+beta = lsq(X_train, y_train)
+y_out = y_output(X_test, beta)
+MSE = MSE(y_test, y_out)
+
+# print the parameters
+print(y_out.shape)
+print(y_test.shape)
+print(MSE)
