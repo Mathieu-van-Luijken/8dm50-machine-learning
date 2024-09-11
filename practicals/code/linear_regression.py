ff4399e4fec09ddf3cef8a9405dd2503fe46602f
JeroenFrie <j.a.w.frieling@student.tue.nl>
2024-09-11 13:32:20 +0200

    knn regression

--- practicals/code/linear_regression.py
+++ practicals/code/linear_regression.py
@@ -19,17 +19,38 @@ def lsq(X, y):
     return beta
 
 def y_output(X, beta):
+    """
+    Determining target vector values for a given Input data matrix and parameter matrix
+    :param X: Input data matrix
+    :param beta: parameter matrix
+    :return: Estimated target vector values
+    """
+    #Add column of ones for the intercept
     ones = np.ones((len(X), 1))
     X = np.concatenate((ones, X), axis=1)
+
+    #Calculate the target vector values
     y_out = np.dot(X, beta)
     return y_out
 
 def MSE(y, y_out):
+    """
+    Calculation of the Mean squared error of two matrices
+    :param y: true target values
+    :param y_out: estimated target values
+    :return: Mean squared error
+    """
+
+    #Calculate the difference between the two input matrices
     error_mat = (y - y_out)
+
+    #Square the difference
     error_mat = np.square(error_mat)
 
+    #Calculate the MSE by summing and dividing by the number of elements
     n = error_mat.shape[0]
-    sum = np.sum(error_mat)
-    mse = sum/n
+    summation = np.sum(error_mat)
+    mse = summation / n
+
     return mse
 
